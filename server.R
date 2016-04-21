d6a1bbf18d1e4840b1ac8f9c55ef8baa79ce3224
Maurizio-Mario <maurizio.murino@hotmail.com>
2016-04-21 12:54:05 +0200

    rchart implemented. Y scales fixed. Problem with grouping.

--- server.R
+++ server.R
@@ -1,27 +1,34 @@
 library(shiny)
-library(xtable)
-library(lattice)
+library(rCharts)
+options(RCHART_LIB = 'nvd3')
+
 dt <- read.table("precipitation_temperature_data.txt", header = TRUE, dec = ".", stringsAsFactors = FALSE)
-
-
-
+        
 shinyServer(function(input, output) {
-        output$chart1 <- renderPlot({
-            validate(
-                need(input$data != "", "Please select your data.")
-            )
+    dt3 <- reactive({                   
         range <- input$year[1] : input$year[2]
         station2 <- input$station
-        dt2 <- dt[dt$Weather_station == station2 & dt$Year %in% range, ]
-        
-        if(input$data=="meanTemp"){
-            p1 <- xyplot(Mean_temperature ~ Year, data = dt2, ylab = "C°", type = "h", lwd = 10)
-            plot(p1)
-            
-        }else if(input$data == "precipitation"){
-            p2 <- xyplot(Precipitation ~ Year, data = dt2, ylab = "Millimeters", type = "h", lwd = 10)
-            plot(p2)
-            
-        }
+        dt2 <- dt[dt$Weather_station == unlist(station2) & dt$Year %in% range, ]
+        dt2 <- as.data.frame(dt2)
+        dt2
     })
+    
+    output$chart1 <- renderChart({
+        validate(
+            need(input$data != "", "Please select your data.")
+        )
+        
+        if(input$data == "precipitation"){
+        n2 <- nPlot(Precipitation ~ Year, data = dt3(), group = "Weather_station", type = 'multiBarChart')
+        n2$chart(forceY = c(0, 2000))
+        }else if(input$data == "meanTemp"){
+            n2 <- nPlot(Mean_temperature ~ Year, data = dt3(), group = "Weather_station", type = 'multiBarChart')
+            n2$chart(forceY = c(0, 15))
+        }
+        n2$set(dom = 'chart1')
+        n2
+
+        
+})
+   
 })
--- ui.R
+++ ui.R
@@ -1,29 +1,27 @@
 library(shiny)
 library(rCharts)
-options(RCHART_LIB = 'polycharts')
+options(RCHART_LIB = 'nvd3')
 
 shinyUI(fluidPage(
-
-  titlePanel("Rain across the Alps"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-        selectInput(inputId = 'station', lab = "Station",
-                    choices = sort(as.character(dt$Weather_station)), 
-                    selected = NULL),
-        sliderInput(inputId = "year", lab = "Years:",
-                    min = 1980,
-                    max = 2011,
-                    value = c(1980, 1985)),
-        checkboxGroupInput(inputId = "data", lab = "Select",
-                           choices = c("Mean temperature" = "meanTemp",
-                                       "Precipitation" = "precipitation"))
-
-    ),
-    mainPanel(
-        h4('Historical data'),
-    plotOutput("chart1")
+    
+    titlePanel("Alpine weather stations"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(inputId = 'station', lab = "Station:",
+                        choices = sort(as.character(dt$Weather_station)), 
+                        selected = NULL),
+            sliderInput(inputId = "year", lab = "Years:",
+                        min = 1980,
+                        max = 2011,
+                        value = c(1980, 1985)),
+            checkboxGroupInput(inputId = "data", lab = "View:",
+                               choices = c("Mean temperature" = "meanTemp",
+                                           "Precipitation" = "precipitation"))
+            
+        ),
+        mainPanel(
+            showOutput("chart1", "nvd3")
+        )
     )
-  )
 ))
